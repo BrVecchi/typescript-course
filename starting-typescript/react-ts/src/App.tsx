890308ff6663b53e83d4215f7f44d4cc167eb982
BrVecchi <bno.vecchi@gmail.com>
2022-02-22 16:37:45 -0300

    until lesson 414

--- starting-typescript/react-ts/src/App.tsx
+++ starting-typescript/react-ts/src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css';
+import { useState } from "react"; 
+
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+import "./App.css";
+import NewTodo from "./components/newTodo";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText)
+
+    setTodos((prevTodos)=>{
+      return prevTodos.concat(newTodo)
+    })
+  }
+
   return (
     <div>
-      
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
     </div>
   );
 }
